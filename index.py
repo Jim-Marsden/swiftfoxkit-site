3466faef07755188562961488ade6b0edaf50262
Jim - Cassia <jamespmarsden@hotmail.com>
2020-03-07 17:17:42 -0800

    updated the css, added a picture, fixed some typos

--- index.py
+++ index.py
@@ -8,13 +8,13 @@ index_page = Blueprint('index_page', __name__,
 @index_page.route('/index')
 @index_page.route('/')
 def index():
-    body_string = "Welcome, we are a small team of developers. Our games are passion projects. While our games are a way from public viewing, we hope you'll like our many offerings. We're currelty building this place, so please don't' mind the dust."
+    body_string = "Welcome, we are a small team of developers. Our games are passion projects. While our games are a way from public viewing, we hope you'll like our many offerings. We're currently building this place, so please don't' mind the dust."
     return render_template('index.html', title="Index", body=body_string.replace('\n', '<br>'))
 
 
 @index_page.route('/about')
 def about():
-    body_string = "Jim Marsden - 'Programmingn'\nInsula - Art, Game Design\nDivi Game Design, Quality Assurance"
+    body_string = "Jim Marsden - Programming\nInsula - Art, Game Design\nDivi - Game Design, Quality Assurance"
     return render_template('index.html', title="About", body=body_string.replace('\n', '<br>'))
 
 
